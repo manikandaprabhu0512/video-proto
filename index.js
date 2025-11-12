0adbad80b3f9b35994511454e266b21c6f62a8d1
mmanikandap0512 <mmanikandap0512@gmail.com>
2025-11-12 16:51:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+export {
+  userPackage,
+  user_protoclient,
+} from "./proto_controllers/user.proto.js";
+export {
+  videoPackage,
+  video_protoclient,
+} from "./proto_controllers/video.proto.js";
